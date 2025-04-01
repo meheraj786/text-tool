8e2976245c77866d6efb79af01d71ca80b2cb3b9
Mehraj Hosen <meherajhosen786@gmail.com>
2025-04-02 01:32:12 +0600

    json done

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const htmlInput= document.querySelector("#html-input")
 const htmlOutput= document.querySelector("#html-output")
 const cssInput= document.querySelector("#css-input")
 const cssOutput= document.querySelector("#css-output")
+const jsonInput= document.querySelector("#json-input")
+const jsonOutput= document.querySelector("#json-output")
+const loremInput= document.querySelector(".lorem-input")
+const loremOutput= document.querySelector("#lorem-text")
 
 function toTitleCase(str) {
   let words = str.split(' ');
@@ -82,6 +86,10 @@ function clearWord() {
 }
 
 function htmlFormat() {
+  if (htmlInput.value=="") {
+    alert("Input Something")
+    
+  }
   const lines= htmlInput.value.split("\n");
   let result = "";
   let indent = 0;
@@ -97,6 +105,10 @@ function htmlFormat() {
   htmlOutput.value= result
 }
 function cssFormat() {
+  if (cssInput.value=="") {
+    alert("Input Something")
+    
+  }
   const lines= cssInput.value.split("\n");
   let result = "";
   let indent = 0;
@@ -110,4 +122,37 @@ function cssFormat() {
     }
   }
   cssOutput.value= result
+}
+
+function jsonFormat() {
+  if (jsonInput.value=="") {
+    alert("Input Something")
+    
+  }
+  const lines= jsonInput.value.split("\n");
+  let result = "";
+  let indent = 0;
+  for (let i = 0; i < lines.length; i++) {
+    if (lines[i].includes("}")) {
+      indent--;
+    }
+    result += "\t".repeat(indent) + lines[i] + "\n";
+    if (lines[i].includes("{")) {
+      indent++;
+    }
+  }
+  jsonOutput.value= result
+}
+
+function generate(){
+  loremOutput.value=""
+  let loremWords= ["lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipisicing", "elit", "quisquam"]
+  let result= ""
+  let loremLength= parseInt(loremInput.value)-3
+  for (let i = 0; i < loremLength; i++) {
+    let index= Math.floor(Math.random()*(loremWords.length));
+    let word = loremWords[index];
+    result+= loremOutput.value + " " + word
+  }
+  loremOutput.value= `Lorem ipsum dolor ${result}`
 }
